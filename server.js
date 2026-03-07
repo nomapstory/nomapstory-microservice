a5ae55933845a68e04f7edef3483fa0e7d6901a7
nomapstory <nomapstory@gmail.com>
2026-03-07 14:35:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -45,8 +45,8 @@ app.get("/pdf", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
